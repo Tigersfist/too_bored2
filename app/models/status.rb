8e1776bdc50c47d9fe736fb12463dd55158558bb
Jordan Martin <jordanmartin.asme@seas.upenn.edu>
2013-09-07 22:50:00 -0400

    Title changes

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,6 +1,6 @@
 class Status < ActiveRecord::Base
   attr_accessible :name, :objecta, :objectb, :objectc, :objectd, :participants
+  GAMES = ["monopoly", "duck duck goose"]
+	#get_game()
 
-
-  
 end
